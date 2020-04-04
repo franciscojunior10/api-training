e9a4e46764dd2e49c7c366d34685d8babc663e09
Francisco Junior <franciscojn49@gmail.com>
2020-04-04 20:47:05 -0300

    feat(project): configs
    configuring .eslintrc.js, .prettierrc and .editorconfig.

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Server OK' });
-})
+  return res.json({ message: 'Server OK' });
+});
 
 export default routes;
--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import 'dotenv/config';
 import app from './app';
 
 app.listen(process.env.PORT, () => {
-    console.log(`Servidor Executando! ... http://localhost:${process.env.PORT}`)
-})
+  console.log(`Servidor Executando! ... http://localhost:${process.env.PORT}`);
+});
