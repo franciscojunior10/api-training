7220b457e2eb7e1fe8d2310d409b1b9e9bbc2336
Francisco Junior <franciscojn49@gmail.com>
2020-04-05 22:19:57 -0300

    feat(controller): created user controller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,23 @@
+import User from '../models/User';
+
+class UserController {
+    async store(req, res) {
+        const emailExists = await User.findOne({
+            where: { email: req.body.email },
+        });
+
+        if (emailExists) {
+            return res.status(400).json({ error: 'Email já existe.' });
+        }
+
+        const { id, name, email } = await User.create(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+        });
+    }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'ok' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
