d899951e4667757bb14eb64755d6cb00c869c57d
Francisco Junior <franciscojn49@gmail.com>
2020-04-30 12:27:04 -0300

    feat(validation): input data validation
    was used to the yup library

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -10,12 +10,23 @@ require('dotenv/config');
 
 const jwt = require('jsonwebtoken');
 
+const Yup = require('yup');
+
 const User = require('../models/User');
 
 const configAuth = require('../../config/auth');
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+            password: Yup.string().min(6).required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails.' });
+        }
+
         const { email, password } = req.body;
 
         const user = await User.findOne({ where: { email } });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,19 @@
 // import User from '../models/User';
+const Yup = require('yup');
 const User = require('../models/User');
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().min(6).required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validation fails.' });
+        }
+
         const emailExists = await User.findOne({
             where: { email: req.body.email },
         });
